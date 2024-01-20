0b05d939963f24ae72d7746077a4f7c6f1738536
Kapilan199 <kapilanr2003@gmail.com>
2024-01-19 16:48:16 -0800

    style html file

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
+@app.route('/scrap', methods = ['POST'])
+def scrap_comments():
+    return render_template('index.html')
