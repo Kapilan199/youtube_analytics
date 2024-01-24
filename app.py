3b90e0773c4f2c163ee52a7dbf44bede12d144ef
Kapilan199 <kapilanr2003@gmail.com>
2024-01-24 09:09:37 -0800

    new

--- app.py
+++ app.py
@@ -639,44 +639,49 @@ def scrap_comments():
     print(dataset)
     ## Separating Positive and Negative Comments
 
-    for (sentiment), group in dataset.groupby(['vader_sentiment']):
-         group.to_csv(f'{sentiment}.csv', index=False)
+    # for (sentiment), group in dataset.groupby(['vader_sentiment']):
+    #      group.to_csv(f'{sentiment}.csv', index=False)
     
-    if checkcsv.exists('1.csv') == False:                             # If 1.csv file does not exist, it creates one empty 1.csv file.
-        with open('1.csv', 'w', encoding='UTF8', newline='') as f1:
-            writer1 = csv.writer(f1)
-            header1 = ['Empty', 'Empty', 'Empty']
-            row1 = ['No Positive Comments', 'No Positive Comments', 'No Positive Comments']
-            writer1.writerow(header1)
-            writer1.writerow(row1)
+    # if checkcsv.exists('1.csv') == False:                             # If 1.csv file does not exist, it creates one empty 1.csv file.
+    #     with open('1.csv', 'w', encoding='UTF8', newline='') as f1:
+    #         writer1 = csv.writer(f1)
+    #         header1 = ['Empty', 'Empty', 'Empty']
+    #         row1 = ['No Positive Comments', 'No Positive Comments', 'No Positive Comments']
+    #         writer1.writerow(header1)
+    #         writer1.writerow(row1)
 
-    if checkcsv.exists('0.csv') == False:                             # If 1.csv file does not exist, it creates one empty 1.csv file.
-        with open('0.csv', 'w',encoding='UTF8', newline='') as f0:
-            writer0 = csv.writer(f0)
-            header0 = ['Empty', 'Empty', 'Empty']
-            row0 = ['No Negative Comments', 'No Negative Comments', 'No Negative Comments']
-            writer0.writerow(header0)
-            writer0.writerow(row0)
+    # if checkcsv.exists('0.csv') == False:                             # If 1.csv file does not exist, it creates one empty 1.csv file.
+    #     with open('0.csv', 'w',encoding='UTF8', newline='') as f0:
+    #         writer0 = csv.writer(f0)
+    #         header0 = ['Empty', 'Empty', 'Empty']
+    #         row0 = ['No Negative Comments', 'No Negative Comments', 'No Negative Comments']
+    #         writer0.writerow(header0)
+    #         writer0.writerow(row0)
     
-    pos = (pd.read_csv("1.csv", engine = 'python')).iloc[:, :-1]
-    neg = (pd.read_csv("0.csv", engine = 'python')).iloc[:, :-1]
+    # pos = (pd.read_csv("1.csv", engine = 'python')).iloc[:, :-1]
+    # neg = (pd.read_csv("0.csv", engine = 'python')).iloc[:, :-1]
      
-    print("check 3")
-    print(pos)
+    # print("check 3")
+    # print(pos)
 
-    positive_comments = pos.to_csv("Positive_Comments.csv", index=False)
-    negative_comments = neg.to_csv("Negative_Comments.csv",index=False)
+    # positive_comments = pos.to_csv("Positive_Comments.csv", index=False)
+    # negative_comments = neg.to_csv("Negative_Comments.csv",index=False)
+
+    positive_comments = dataset[dataset['vader_sentiment'] == 1]
+    negative_comments = dataset[dataset['vader_sentiment'] == 0]
 
     print("check 4")
     print(positive_comments)
 
-    video_positive_comments = str(len(pos.axes[0])) + ' Comments'  #Finding total rows in positive comments
-    video_negative_comments = str(len(neg.axes[0])) + ' Comments'  #Finding total rows in negative comments
+    video_positive_comments = str(len(positive_comments)) + ' Comments'  #Finding total rows in positive comments
+    video_negative_comments = str(len(negative_comments)) + ' Comments'  #Finding total rows in negative comments
     
-    if (pd.read_csv('1.csv', nrows=0).columns.tolist())[0] == 'Empty':
-        video_positive_comments = '0 Comments'
-    if (pd.read_csv('0.csv', nrows=0).columns.tolist())[0] == 'Empty':
-        video_negative_comments = '0 Comments'
+
+
+    # if (pd.read_csv('1.csv', nrows=0).columns.tolist())[0] == 'Empty':
+    #     video_positive_comments = '0 Comments'
+    # if (pd.read_csv('0.csv', nrows=0).columns.tolist())[0] == 'Empty':
+    #     video_negative_comments = '0 Comments'
     print("testing full comments")
     print(positive_comments, negative_comments, video_positive_comments, video_negative_comments)    
 
@@ -747,7 +752,7 @@ def scrap_comments():
     pos_comments_csv = pd.read_csv('Positive_Comments.csv')
     neg_comments_csv = pd.read_csv('Negative_Comments.csv')
     print("File checking contents:")
-    print(list_sentiment)
+    print(pos_comments_csv)
     print("File checking contents:")
 
 
